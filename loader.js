c6035e3b23dc04ec7f6bc20d67947ee31578fb1f
Alex PreusX <preusx.dev@gmail.com>
2015-12-14 09:37:55 +0200

    Base configuration added.

---
+++ loader.js
@@ -0,0 +1,16 @@
+global.DEBUG    = false;
+global.WATCHING = false;
+
+global.P = {
+  styl  : 'stylus/',
+  css   : 'css/',
+  jade  : 'jade/',
+  html  : '',
+  js    : 'js/',
+  img   : 'img/',
+
+  font  : 'fonts/',
+
+  dev   : './dev/',
+  dist  : './dist/',
+};
