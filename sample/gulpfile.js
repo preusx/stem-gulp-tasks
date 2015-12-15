2cc84425f68ae76cee441ad145193d43b6b656d7
Alex PreusX <preusx.dev@gmail.com>
2015-12-15 17:40:35 +0200

    Sample gulpfile added.

---
+++ sample/gulpfile.js
@@ -0,0 +1,46 @@
+var loader = require('../loader.js');
+
+loader(__dirname, [
+    'copy-root.js',
+    'copy-css.js',
+    'copy-fonts.js',
+    'copy-img.js',
+    'copy-php.js',
+    'copy-python.js',
+
+    'jade-html.js',
+    'jade-php.js',
+
+    'stylus-style.js',
+
+    'js-base.js',
+  ]);
+
+
+/**
+ * Task groups
+ */
+
+gulp.task('copy', [
+    'copy-root:build',
+    'copy-css:build',
+    'copy-fonts:build',
+    'copy-img:build',
+    'copy-php:build',
+    'copy-python:build',
+  ]);
+
+gulp.task('build', [
+    'copy',
+
+    'jade-html:build',
+    'jade-php:build',
+
+    'stylus-style:build',
+
+    'js-base:build',
+  ]);
+
+gulp.task('dist', ['build']);
+
+gulp.task('default', []);
