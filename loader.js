fb9b10515bb9d6203aba1a91d20350110f252813
Alex PreusX <preusx.dev@gmail.com>
2015-12-15 17:40:18 +0200

    Task loader added.

--- loader.js
+++ loader.js
@@ -1,3 +1,8 @@
+var path = require('path');
+
+var gulp  = require('gulp');
+var watch = require('gulp-watch');
+
 global.DEBUG    = false;
 global.WATCHING = false;
 
@@ -13,4 +18,61 @@ global.P = {
 
   dev   : './dev/',
   dist  : './dist/',
+};
+
+
+module.exports = function loader(dir, taskPaths) {
+  var tasks = {};
+
+  /**
+   * Loading tasks
+   */
+  for(var i = 0, l = taskPaths.length; i < l; i++) {
+    var taskPath = taskPaths[i],
+        taskName = path.basename(taskPath, '.js');
+    var spos = taskPath.indexOf('./');
+    var rpath;
+
+    if(spos == 0 || spos == 1) {
+      rpath = path.normalize(dir + taskPath);
+    } else {
+      rpath = path.normalize(__dirname + '/lib/tasks/' + taskPath);
+    }
+
+    var task = tasks[taskName] = require(rpath);
+
+    gulp.task(taskName + ':dev', task.dev);
+    gulp.task(taskName + ':build', task.build);
+
+    if(typeof task.watch == 'function') {
+      gulp.task(taskName + ':watch', task.watch);
+    }
+  }
+
+  /**
+   * Additive tasks.
+   */
+  gulp.task('watch', function(){
+    console.log('I\'m watching...');
+
+    global.WATCHING = true;
+
+    for(var i in tasks) {
+      if(!tasks.hasOwnProperty(i)) continue;
+
+      var task = tasks[i];
+
+      if(task.watch) {
+        if(typeof task.watch == 'function') {
+          gulp.start(i + ':watch');
+        } else {
+          watch(task.watch, (function(taskName) {
+            return function() {
+              gulp.start(taskName + ':dev');
+            };
+          })(i));
+        }
+      }
+    }
+  });
 };
